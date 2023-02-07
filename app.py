4c0c32009c0cf4125080a4ed62223bf40176e1c0
jazgamarra <j444min@gmail.com>
2023-02-07 15:39:04 -0300

    >:c

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect, url_for 
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
@@ -19,7 +19,7 @@ login_manager.login_view = 'login'
 # ----------------------------------------------------------------------
 # MODELOS
 # ----------------------------------------------------------------------
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
@@ -43,6 +43,7 @@ class ToDo (db.Model):
 # ----------------------------------------------------------------------
 # FORMULARIOS
 # ----------------------------------------------------------------------
+
 class RegisterForm(FlaskForm): 
     username = StringField('Username')
     email = StringField('Email')
@@ -54,9 +55,9 @@ class LoginForm (FlaskForm):
     password = PasswordField('Password')
     submit = SubmitField('Sign In')
 
-# ----------------------------------------------------------------------
-# RUTAS
-# ----------------------------------------------------------------------
+# Crear tablas en sql alchemy 
+with app.app_context():
+    db.create_all()
 
 # Crear la sesion del usuario 
 # Hace un query a la base de datos y busca el usuario con el email que se ingreso 
@@ -64,24 +65,45 @@ class LoginForm (FlaskForm):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Crear tablas en sql alchemy 
-with app.app_context():
-    db.create_all()
+# ----------------------------------------------------------------------
+# RUTAS
+# ----------------------------------------------------------------------
+@app.route ('/')
+def home ():
+    return render_template('home.html')
 
-@app.route ('/', methods=['GET', 'POST'])
+@app.route ('/register', methods=['GET', 'POST'])
 def register ():
     form = RegisterForm()
     if form.validate_on_submit():
         user = User(name=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        return f'SE CREO EL USUARIO {user}'
+        return render_template('todo.html')
     return render_template('register.html', form=form)
 
 @app.route ('/login', methods=['GET', 'POST'])
 def login ():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        user_to_be_logged = User.query.filter_by(email=form.email.data).first() 
+        if user_to_be_logged: 
+            login_user(user_to_be_logged)
+            return render_template('todo.html') 
     return render_template('login.html', form=form)
+
+@app.route ('/todo', methods=['GET', 'POST'])
+@login_required
+def todo ():
+    return render_template('todo.html') 
+
+@app.route ('/logout')
+@login_required
+def logout ():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
