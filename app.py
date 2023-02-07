e5805cbe7ac413d1f5105a3df7844b937a61f588
jazgamarra <j444min@gmail.com>
2023-02-07 12:06:06 -0300

    formulario de login

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template 
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 # Crear el servidor 
 app = Flask(__name__)
@@ -10,7 +10,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'jamin'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+# Si no esta logeado, le referencia a esta ruta
+login_manager.login_view = 'login' 
 
+
+# ----------------------------------------------------------------------
+# MODELOS
+# ----------------------------------------------------------------------
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(100))
@@ -22,14 +30,43 @@ class User(db.Model):
         self.email = email
         self.password = password
 
+class ToDo (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, description, user_id):
+        self.title = title
+        self.description = description
+        self.user_id = user_id
+
+# ----------------------------------------------------------------------
+# FORMULARIOS
+# ----------------------------------------------------------------------
 class RegisterForm(FlaskForm): 
     username = StringField('Username')
     email = StringField('Email')
     password = PasswordField('Password')
     submit = SubmitField('Sign Up')
 
+class LoginForm (FlaskForm):
+    email = EmailField('Email')
+    password = PasswordField('Password')
+    submit = SubmitField('Sign In')
 
+# ----------------------------------------------------------------------
+# RUTAS
+# ----------------------------------------------------------------------
 
+# Crear la sesion del usuario 
+# Hace un query a la base de datos y busca el usuario con el email que se ingreso 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Crear tablas en sql alchemy 
+with app.app_context():
+    db.create_all()
 
 @app.route ('/', methods=['GET', 'POST'])
 def register ():
@@ -41,10 +78,10 @@ def register ():
         return f'SE CREO EL USUARIO {user}'
     return render_template('register.html', form=form)
 
-
-# Crear tablas en sql alchemy 
-with app.app_context():
-    db.create_all()
+@app.route ('/login', methods=['GET', 'POST'])
+def login ():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/penguin/taller_feliz_backend/venv"
+set -gx VIRTUAL_ENV "/home/penguin/Escritorio/taller_backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
